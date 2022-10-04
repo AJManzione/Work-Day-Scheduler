b86f72f653a40a0db50bc1d7104657b1e28ed562
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-10-04 00:08:49 -0600

    html layout and css style - basic for structure

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+$("#date-my").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+
+var NineAm
+var TenAm
+var ElevenAm
+var TwelvePm
+var OnePm
+var TwoPm
+var ThreePm
+var FourPm
+var FivePm
